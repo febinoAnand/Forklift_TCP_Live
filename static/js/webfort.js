9ba9e489a09e296aa6e9118beee66f13b8684143
karthik <karthik202m@gmail.com>
2024-03-11 18:20:16 +0530

    Changed pie Chart

--- static/js/webfort.js
+++ static/js/webfort.js
@@ -1,48 +1,75 @@
 // pie chart start//
-var ctx = document.getElementById('myPieChart').getContext('2d');
-var myPieChart;
-
-function updatePieChart() {
-    fetch('/get_today_gps_data/')
-        .then(response => response.json())
-        .then(data => {
-            if (data.active === 0 && data.idle === 0 && data.inactive === 0) {
-                data.inactive = 100;
-            }
-
-            myPieChart.data.datasets[0].data = [data.active, data.inactive, data.idle];
-            myPieChart.update();
-        })
-        .catch(error => console.error('Error fetching data:', error));
-}
 document.addEventListener('DOMContentLoaded', function() {
-    myPieChart = new Chart(ctx, {
-        type: 'pie',
-        data: {
-            labels: ['Active Vehicles', 'Inactive Vehicles', 'Idle Vehicles'],
-            datasets: [{
-                label: 'Today',
-                data: [0, 0, 0],
-                backgroundColor: [
-                    'rgba(54, 162, 235, 0.8)',
-                    'rgba(255, 99, 132, 0.8)',
-                    'rgba(255, 206, 86, 0.8)'
-                ],
-                borderColor: [
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(255, 206, 86, 1)'
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            responsive: true
-        }
-    });
+  var ctx = document.getElementById('myPieChart').getContext('2d');
+  var myPieChart;
 
-    updatePieChart();
-    setInterval(updatePieChart, 5000);
+  function updatePieChart() {
+      var todayMorningTimestamp = new Date();
+      todayMorningTimestamp.setHours(0, 0, 0, 0);
+      var currentTimestamp = Math.floor(Date.now() / 1000);
+      fetch('/get_today_gps_data/?start=' + todayMorningTimestamp.getTime() / 1000 + '&end=' + currentTimestamp)
+          .then(response => {
+              if (!response.ok) {
+                  throw new Error('Network response was not ok');
+              }
+              return response.json();
+          })
+          .then(data => {
+              var activeHours = 0;
+              var inactiveHours = 0;
+              var idleHours = 0;
+
+              if (data.length === 0) {
+                  inactiveHours = 24;
+              } else {
+                  data.forEach(entry => {
+                      if (entry.state === 'active') {
+                          activeHours += entry.duration / 3600;
+                      } else if (entry.state === 'inactive') {
+                          inactiveHours += entry.duration / 3600;
+                      } else if (entry.state === 'idle') {
+                          idleHours += entry.duration / 3600;
+                      }
+                  });
+              }
+
+              myPieChart.data.datasets[0].data = [activeHours, inactiveHours, idleHours];
+              myPieChart.update();
+          })
+          .catch(error => {
+              console.error('Error fetching or parsing data:', error);
+              myPieChart.data.datasets[0].data = [0, 24, 0];
+              myPieChart.update();
+          });
+  }
+
+  myPieChart = new Chart(ctx, {
+      type: 'pie',
+      data: {
+          labels: ['Active Hours', 'Inactive Hours', 'Idle Hours'],
+          datasets: [{
+              label: 'Today',
+              data: [0, 0, 0],
+              backgroundColor: [
+                'rgba(54, 162, 235, 0.8)',
+                'rgba(255, 99, 132, 0.8)',
+                'rgba(255, 206, 86, 0.8)'
+            ],
+            borderColor: [
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 206, 86, 1)'
+            ],
+              borderWidth: 1
+          }]
+      },
+      options: {
+          responsive: true
+      }
+  });
+
+  updatePieChart();
+  setInterval(updatePieChart, 5000);
 });
 // pie chart end//
 
@@ -119,7 +146,7 @@ function updateBarChart(data) {
       } else if (percent > 33.33 && percent <= 66.66) {
         bar.style.backgroundColor = '#5BADFF';
       } else if (percent > 66.66 && percent <= 100) {
-        bar.style.backgroundColor = '#FBFC9C';
+        bar.style.backgroundColor = '#FF6E26';
       }
     }
 }
