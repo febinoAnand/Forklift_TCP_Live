583aede4542947935bd53ddde65c723efd7508aa
karthik <karthik202m@gmail.com>
2024-03-26 11:23:08 +0530

    changed progress

--- static/js/webfort.js
+++ static/js/webfort.js
@@ -1,77 +1,80 @@
 // pie chart start//
 document.addEventListener('DOMContentLoaded', async () =>  {
-  const ctx = document.getElementById('myPieChart').getContext('2d');
-  let myPieChart;
-
-  const updatePieChart = async () => {
-      try {
-          const todayMorningTimestamp = new Date();
-          todayMorningTimestamp.setHours(0, 0, 0, 0);
-          const currentTimestamp = Math.floor(Date.now() / 1000);
-
-          const response = await fetch(`/get_today_gps_data/?start=${todayMorningTimestamp.getTime() / 1000}&end=${currentTimestamp}`);
-          if (!response.ok) {
-              throw new Error('Network response was not ok');
-          }
-
-          const data = await response.json();
-          let activeHours = 0, inactiveHours = 0, idleHours = 0;
-
-          if (data.length === 0) {
-              inactiveHours = 24;
-          } else {
-              data.forEach(entry => {
-                  switch (entry.state) {
-                      case 1:
-                          activeHours += entry.duration / 3600;
+    const ctx = document.getElementById('myPieChart').getContext('2d');
+    let myPieChart;
+    const updatePieChart = async () => {
+        try {
+            const todayMorningTimestamp = new Date();
+            todayMorningTimestamp.setHours(0, 0, 0, 0);
+            const currentTimestamp = Math.floor(Date.now() / 1000);
+            const response = await fetch(`/get_today_gps_data/?start=${todayMorningTimestamp.getTime() / 1000}&end=${currentTimestamp}`);
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+          
+            const data = await response.json();
+            let activeHours = 0, inactiveHours = 0, idleHours = 0;
+  
+            if (data.length === 0) {
+                inactiveHours = 24;
+            } else {
+                data.forEach(entry => {
+                    switch (entry.state) {
+                      case "Active":
+                          activeHours = entry.duration;
                           break;
-                      case 2:
-                          inactiveHours += entry.duration / 3600;
+                      case "Inactive":
+                          inactiveHours = entry.duration ;
                           break;
-                      case 3:
-                          idleHours += entry.duration / 3600;
+                      case "Idle":
+                          idleHours = entry.duration;
                           break;
-                  }
-              });
-          }
-
-          myPieChart.data.datasets[0].data = [activeHours, inactiveHours, idleHours];
-          myPieChart.update();
-      } catch (error) {
-          console.error('Error fetching or parsing data:', error);
-          myPieChart.data.datasets[0].data = [0, 24, 0];
-          myPieChart.update();
-      }
-  };
-
-  myPieChart = new Chart(ctx, {
-      type: 'pie',
-      data: {
-          labels: ['Active Hours', 'Inactive Hours', 'Idle Hours'],
-          datasets: [{
-              label: 'Today',
-              data: [0, 0, 0],
-              backgroundColor: [
-                  'rgba(54, 162, 235, 0.8)',
-                  'rgba(255, 99, 132, 0.8)',
-                  'rgba(255, 206, 86, 0.8)'
-              ],
-              borderColor: [
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(255, 99, 132, 1)',
-                  'rgba(255, 206, 86, 1)'
-              ],
-              borderWidth: 1
-          }]
-      },
-      options: {
-          responsive: true
-      }
+                      case "Alert":
+                          inactiveHours += entry.duration;
+                          break;
+                    }
+                });
+  
+  
+  
+  
+            }
+  
+            myPieChart.data.datasets[0].data = [activeHours, inactiveHours, idleHours];
+            myPieChart.update();
+        } catch (error) {
+            console.error('Error fetching or parsing data:', error);
+            myPieChart.data.datasets[0].data = [0, 24, 0];
+            myPieChart.update();
+        }
+    };
+    myPieChart = new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: ['Active Hours', 'Inactive Hours', 'Idle Hours'],
+            datasets: [{
+                label: 'Today',
+                data: [0, 0, 0],
+                backgroundColor: [
+                    'rgba(54, 162, 235, 0.8)',
+                    'rgba(255, 99, 132, 0.8)',
+                    'rgba(255, 206, 86, 0.8)'
+                ],
+                borderColor: [
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(255, 206, 86, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            responsive: true
+        }
+    });
+    updatePieChart();
+    setInterval(updatePieChart, 5000);
   });
-
-  updatePieChart();
-  setInterval(updatePieChart, 5000);
-});
 // pie chart end//
 
 // line chart start //
