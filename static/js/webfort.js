5307d52a0ad98d2b2488b56313094197cb414a3c
karthik <karthik202m@gmail.com>
2024-03-27 17:32:38 +0530

    Changed Map Position

--- static/js/webfort.js
+++ static/js/webfort.js
@@ -1,41 +1,41 @@
 // pie chart start//
 document.addEventListener('DOMContentLoaded', async () =>  {
-  const ctx = document.getElementById('myPieChart').getContext('2d');
-  let myPieChart;
+    const ctx = document.getElementById('myPieChart').getContext('2d');
+    let myPieChart;
 
-  const updatePieChart = async () => {
-      try {
-          const todayMorningTimestamp = new Date();
-          todayMorningTimestamp.setHours(0, 0, 0, 0);
-          const currentTimestamp = Math.floor(Date.now() / 1000);
+    const updatePieChart = async (deviceID) => {
+        try {
+            const todayMorningTimestamp = new Date();
+            todayMorningTimestamp.setHours(0, 0, 0, 0);
+            const currentTimestamp = Math.floor(Date.now() / 1000);
 
-          const response = await fetch(`/get_today_gps_data/?start=${todayMorningTimestamp.getTime() / 1000}&end=${currentTimestamp}`);
-          if (!response.ok) {
-              throw new Error('Network response was not ok');
-          }
+            const response = await fetch(`/get_today_gps_data/?start=${todayMorningTimestamp.getTime() / 1000}&end=${currentTimestamp}&deviceID=${deviceID}`);
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
         
-          const data = await response.json();
-          let activeHours = 0, inactiveHours = 0, idleHours = 0;
+            const data = await response.json();
+            let activeHours = 0, inactiveHours = 0, idleHours = 0;
 
-          if (data.length === 0) {
-              inactiveHours = 24;
-          } else {
-            //   data.forEach(entry => {
-            //       switch (entry.state) {
-            //           case 1:
-            //               activeHours += entry.duration / 3600;
-            //               break;
-            //           case 2:
-            //               inactiveHours += entry.duration / 3600;
-            //               break;
-            //           case 3:
-            //               idleHours += entry.duration / 3600;
-            //               break;
-            //       }
-            //   });
-            
+            if (data.length === 0) {
+                inactiveHours = 24;
+            } else {
+              //   data.forEach(entry => {
+              //       switch (entry.state) {
+              //           case 1:
+              //               activeHours += entry.duration / 3600;
+              //               break;
+              //           case 2:
+              //               inactiveHours += entry.duration / 3600;
+              //               break;
+              //           case 3:
+              //               idleHours += entry.duration / 3600;
+              //               break;
+              //       }
+              //   });
+              
               data.forEach(entry => {
-                  switch (entry.state) {
+                switch (entry.state) {
                     case "Active":
                         activeHours = entry.duration;
                         break;
@@ -48,158 +48,154 @@ document.addEventListener('DOMContentLoaded', async () =>  {
                     case "Alert":
                         inactiveHours += entry.duration;
                         break;
-                  }
-              });
+                }
+            });
+        }
 
+        myPieChart.data.datasets[0].data = [activeHours, inactiveHours, idleHours];
+        myPieChart.update();
+    } catch (error) {
+        console.error('Error fetching or parsing data:', error);
+        myPieChart.data.datasets[0].data = [0, 24, 0];
+        myPieChart.update();
+    }
+};
 
-
-
-          }
-
-          myPieChart.data.datasets[0].data = [activeHours, inactiveHours, idleHours];
-          myPieChart.update();
-      } catch (error) {
-          console.error('Error fetching or parsing data:', error);
-          myPieChart.data.datasets[0].data = [0, 24, 0];
-          myPieChart.update();
-      }
-  };
-
-  myPieChart = new Chart(ctx, {
-      type: 'pie',
-      data: {
-          labels: ['Active Hours', 'Inactive Hours', 'Idle Hours'],
-          datasets: [{
-              label: 'Today',
-              data: [0, 0, 0],
-              backgroundColor: [
-                  'rgba(54, 162, 235, 0.8)',
-                  'rgba(255, 99, 132, 0.8)',
-                  'rgba(255, 206, 86, 0.8)'
-              ],
-              borderColor: [
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(255, 99, 132, 1)',
-                  'rgba(255, 206, 86, 1)'
-              ],
-              borderWidth: 1
-          }]
-      },
-      options: {
-          responsive: true
-      }
-  });
-
-  updatePieChart();
-  setInterval(updatePieChart, 5000);
-});
-// pie chart end//
-
-// line chart start //
-document.addEventListener('DOMContentLoaded', function() {
-  var ctx = document.getElementById('myChart').getContext('2d');
-  var myChart;
-
-  function updateChart() {
-    $.ajax({
-      url: '/api/get_last_data/',
-      success: function(data) {
-        var gpsData = data.gps_data.map(item => item.speed);
-        var extData = data.ext_data.map(item => item.speed);
-        var labels = data.gps_data.map(item => item.time);
-
-        myChart.data.labels = labels;
-        myChart.data.datasets[0].data = gpsData;
-        myChart.data.datasets[1].data = extData;
-
-        myChart.update();
-      }
-    });
-  }
-
-  myChart = new Chart(ctx, {
-    type: 'line',
+myPieChart = new Chart(ctx, {
+    type: 'pie',
     data: {
-      labels: [],
-      datasets: [{
-        label: 'GPS-Speed',
-        data: [],
-        borderColor: 'red',
-        borderWidth: 1
-      },
-      {
-        label: 'EXT-Speed',
-        data: [],
-        borderColor: 'blue',
-        borderWidth: 1
-      }]
+        labels: ['Active Hours', 'Inactive Hours', 'Idle Hours'],
+        datasets: [{
+            label: 'Today',
+            data: [0, 0, 0],
+            backgroundColor: [
+                'rgba(54, 162, 235, 0.8)',
+                'rgba(255, 99, 132, 0.8)',
+                'rgba(255, 206, 86, 0.8)'
+            ],
+            borderColor: [
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 206, 86, 1)'
+            ],
+            borderWidth: 1
+        }]
     },
     options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
-      }
+        responsive: true
     }
-  });
-
-  updateChart();
-  setInterval(updateChart, 5000);
 });
-//line chart end //
 
-// bar chart start //
-function updateBarChart(data) {
-    Object.keys(data).forEach(day => {
-        if (day.toLowerCase() !== 'sunday') { 
-            var utilizationData = data[day];
-            var row = document.querySelector(`.bar-${day.toLowerCase()}`);
-            if (row) {
-                var bar = row.querySelector('.bar');
-                if (bar) {
-                    for (const state in utilizationData) {
-                        if (state !== 'Total') {
-                            var element = bar.querySelector(`.${state.toLowerCase()}`);
-                            if (element) {
-                                var hours = parseFloat(utilizationData[state]);
-                                if (!isNaN(hours) && hours > 0) { 
-                                    element.textContent = hours.toFixed(2);
-                                    element.style.height = `${hours}px`;
-                                    if (state === 'Active') {
-                                        element.style.backgroundColor = '#6EC7FA';
-                                    } else if (state === 'Inactive') {
-                                        element.style.backgroundColor = '#FA766E';
-                                    } else if (state === 'Idle') {
-                                        element.style.backgroundColor = '#FAFA6E';
-                                    }
-                                } else {
-                                    element.textContent = '';
-                                    element.style.height = '0px';
-                                }
-                            }
-                        }
-                    }
-                } else {
-                    console.error(`Bar not found for ${day}.`);
+updatePieChart("device_id");
+setInterval(() => updatePieChart("device_id"), 5000);
+});
+  // pie chart end//
+  
+  // line chart start //
+  document.addEventListener('DOMContentLoaded', function() {
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var myChart;
+
+    function updateChart(deviceID) {
+        $.ajax({
+            url: '/api/get_last_data/?deviceID=' + deviceID,
+            success: function(data) {
+                var gpsData = data.gps_data.map(item => item.speed);
+                var extData = data.ext_data.map(item => item.speed);
+                var labels = data.gps_data.map(item => item.time);
+
+                myChart.data.labels = labels;
+                myChart.data.datasets[0].data = gpsData;
+                myChart.data.datasets[1].data = extData;
+
+                myChart.update();
+            }
+        });
+    }
+
+    myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: [],
+            datasets: [{
+                    label: 'GPS-Speed',
+                    data: [],
+                    borderColor: 'red',
+                    borderWidth: 1
+                },
+                {
+                    label: 'EXT-Speed',
+                    data: [],
+                    borderColor: 'blue',
+                    borderWidth: 1
                 }
-            } else {
-                console.error(`Row for ${day} not found.`);
+            ]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
             }
         }
     });
-}
-function fetchDataAndUpdate() {
-    fetch('/get_utilization_hours/')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => updateBarChart(data))
-        .catch(error => console.error('Error fetching data:', error));
-}
-
-fetchDataAndUpdate();
-setInterval(fetchDataAndUpdate, 5000);
-// bar chart ens //
+    updateChart("device_id");
+    setInterval(() => updateChart("device_id"), 5000);
+});
+  //line chart end //
+  
+  // bar chart start //
+  function updateBarChart(data) {
+      Object.keys(data).forEach(day => {
+          if (day.toLowerCase() !== 'sunday') { 
+              var utilizationData = data[day];
+              var row = document.querySelector(`.bar-${day.toLowerCase()}`);
+              if (row) {
+                  var bar = row.querySelector('.bar');
+                  if (bar) {
+                      for (const state in utilizationData) {
+                          if (state !== 'Total') {
+                              var element = bar.querySelector(`.${state.toLowerCase()}`);
+                              if (element) {
+                                  var hours = parseFloat(utilizationData[state]);
+                                  if (!isNaN(hours) && hours > 0) { 
+                                      element.textContent = hours.toFixed(1);
+                                      element.style.height = `${hours}px`;
+                                      if (state === 'Active') {
+                                          element.style.backgroundColor = '#6EC7FA';
+                                      } else if (state === 'Inactive') {
+                                          element.style.backgroundColor = '#FA766E';
+                                      } else if (state === 'Idle') {
+                                          element.style.backgroundColor = '#FAFA6E';
+                                      }
+                                  } else {
+                                      element.textContent = '';
+                                      element.style.height = '0px';
+                                  }
+                              }
+                          }
+                      }
+                  } else {
+                      console.error(`Bar not found for ${day}.`);
+                  }
+              } else {
+                  console.error(`Row for ${day} not found.`);
+              }
+          }
+      });
+  }
+  function fetchDataAndUpdate() {
+      fetch('/get_utilization_hours/')
+          .then(response => {
+              if (!response.ok) {
+                  throw new Error('Network response was not ok');
+              }
+              return response.json();
+          })
+          .then(data => updateBarChart(data))
+          .catch(error => console.error('Error fetching data:', error));
+  }
+  
+  fetchDataAndUpdate();
+  setInterval(fetchDataAndUpdate, 5000);
+  // bar chart ens //
