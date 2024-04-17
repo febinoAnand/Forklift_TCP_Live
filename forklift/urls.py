3cd25981ababa4e3cbb89a54c73821eb05c6d8a3
karthik <karthik202m@gmail.com>
2024-04-17 15:56:49 +0530

    changed

--- forklift/urls.py
+++ forklift/urls.py
@@ -1,21 +1,21 @@
 from django.urls import path
-from .views import AllDeviceClass, loginView, deviceDashborad, historypage,get_gps_data_for_date, get_state_data, updateGPSTabledateView,updateEXTTabledateView , updateGPSTableView,updateEXTTableView, report_page_view, register_device ,list_page_view, tracker_device_list,logoutView
+from .views import loginView, deviceDashborad, historypage,get_gps_data_for_date, get_state_data, updateGPSTabledateView,updateEXTTabledateView , updateGPSTableView,updateEXTTableView, report_page_view, register_device ,list_page_view, tracker_device_list,logoutView
 
 
 urlpatterns = [
     path('',loginView, name='login'),
     path('login',loginView, name='login'),
-    path('devicedashboard',AllDeviceClass.as_view(), name='device_dashboard'),
-    path('updategpstable', AllDeviceClass.as_view(), name='update_gpstable'),
-    path('updateexttable', AllDeviceClass.as_view(), name='update_exttable'),
-    path('reportpage/', AllDeviceClass.as_view(), name='report_page'),
-    path('listpage/api/tracker-devices/', AllDeviceClass.as_view(), name='tracker_devices_api'),
-    path('register/', AllDeviceClass.as_view(), name='register_device'),
-    path('listpage/', AllDeviceClass.as_view(), name='list_page'),
+    path('devicedashboard',deviceDashborad, name='device_dashboard'),
+    path('updategpstable',updateGPSTableView, name='update_gpstable'),
+    path('updateexttable',updateEXTTableView, name='update_exttable'),
+    path('reportpage/', report_page_view, name='report_page'),
+    path('register/', register_device, name='register_device'),
+    path('listpage/', list_page_view, name='list_page'),
+    path('listpage/api/tracker-devices/', tracker_device_list, name='tracker-device-list'),
     path('logout',logoutView,name='logoutView'),
-    path('historypage',AllDeviceClass.as_view(), name='history_page'),
-    path('get_gps_data_for_date/', AllDeviceClass.as_view(), name='get_gps_data_for_date'),
-    path('updateextdatetable',AllDeviceClass.as_view(), name='update_exttabledate'),
-    path('updategpsdatetable',AllDeviceClass.as_view(), name='update_gpsdatetable'),
+    path('historypage',historypage, name='history_page'),
+    path('get_gps_data_for_date/', get_gps_data_for_date, name='get_gps_data_for_date'),
+    path('updateextdatetable',updateEXTTabledateView, name='update_exttabledate'),
+    path('updategpsdatetable',updateGPSTabledateView, name='update_gpsdatetable'),
     path('get_state_data/', get_state_data, name='get_state_data'),
 ]
